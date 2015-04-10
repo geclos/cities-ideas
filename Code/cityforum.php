c9c24a7d0aaa386c0f148991970d2f59905386fc
Gerard Clos <clos.gerard@gmail.com>
2015-04-10 11:26:59 +0200

    10/4/15

---
+++ Code/cityforum.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Template Name: The universal forum of cultures
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap clearfix">
+
+						<div id="main" class="first clearfix" role="main">
+
+							<header class="row">
+								<img src="<?php echo get_template_directory_uri();?>/library/images/cityforum_header.png" alt="Monterrey skyline">
+								<div class="post-meta centered">
+									<div class="meta">
+										<p>#</p>
+									</div>
+								</div>
+							</header>
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class('tencol centered clearfix'); ?> role="article">
+
+								<header class="article-header">
+
+									<h1 class="page-title"><?php the_title();?></h1>
+
+								</header> <!-- end article header -->
+
+								<section class="entry-content">
+									<?php the_content(); ?>
+								</section> <!-- end article section -->
+
+								<footer class="article-footer">
+									<p class="clearfix"><?php the_tags('<span class="tags">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?></p>
+
+								</footer> <!-- end article footer -->
+
+							</article> <!-- end article -->
+
+							<?php endwhile; ?>
+
+							<?php else : ?>
+
+									<article id="post-not-found" class="hentry clearfix">
+											<header class="article-header">
+												<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
+										</header>
+											<section class="entry-content">
+												<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e("This is the error message in the page-custom.php template.", "bonestheme"); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+						</div> <!-- end #main -->
+
+				</div> <!-- end #inner-content -->
+
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
